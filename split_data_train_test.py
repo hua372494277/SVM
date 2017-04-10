9d7e2061e2952af6129b7dcbb550d9e968df52d3
hua372494277 <huayongpan@gmail.com>
2017-04-10 19:46:40 +0800

    Add: merge and splite dataset: positive and negative data set/training and test data set

---
+++ split_data_train_test.py
@@ -0,0 +1,7 @@
+#split all data into 2 sets: training set and test set
+def merge_data(positive_set, negative_set):
+    
+    return data_set
+
+def split_data():
+    return training_set, test_set
