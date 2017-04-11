8244a5496e6a8e85d637f7bcd6931973968faa22
hua372494277 <huayongpan@gmail.com>
2017-04-11 11:21:47 +0800

    Fix: Rename the name of file to include other functions

--- split_data_train_test.py
+++
@@ -1,7 +0,0 @@
-#split all data into 2 sets: training set and test set
-def merge_data(positive_set, negative_set):
-    
-    return data_set
-
-def split_data():
-    return training_set, test_set
